caabaf1d5359711d1955f0b2a527436a3d4a4568
wangmingzhu <wangmingzhu@qince.com>
2024-07-18 17:53:16 +0800

    feat: encapsulation commandSeeds

--- extension.js
+++ extension.js
@@ -1,10 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode')
 
-const components = require('./utils/components')
-const insertComponent = require('./utils/insertComponent')
-const insertImport = require('./utils/insertImport')
+const commandSeeds = require('./utils/commandSeeds')
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -20,21 +17,7 @@ function activate(context) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand('seeds', function () {
-    // 弹出选择列表
-    vscode.window.showQuickPick(Object.keys(components)).then(async (componentName) => {
-      if (!componentName) {
-        return
-      }
-      // 插入import
-      insertImport(componentName)
-      // 插入代码
-      const item = components[componentName]
-      insertComponent(item.code)
-    })
-  })
-
-  context.subscriptions.push(disposable)
+  commandSeeds(context)
 }
 
 // This method is called when your extension is deactivated
