f905477d315690e566ceb5d547bf7953376bb3fa
wangmingzhu <wangmingzhu@qince.com>
2024-08-01 09:45:01 +0800

    feat: develop the insertDirectory

--- utils/command/insertDirectory/index.js
+++ utils/command/insertDirectory/index.js
@@ -1,6 +1,27 @@
 const vscode = require('vscode')
 const path = require('path')
-// const fs = require('fs-extra')
+
+const fs = require('fs')
+
+// Recursion directory
+function copyDirectory(source, dest) {
+  if (!fs.existsSync(dest)) {
+    fs.mkdirSync(dest, { recursive: true })
+  }
+
+  const entries = fs.readdirSync(source, { withFileTypes: true })
+
+  for (let entry of entries) {
+    const sourcePath = path.join(source, entry.name)
+    const destPath = path.join(dest, entry.name)
+
+    if (entry.isDirectory()) {
+      copyDirectory(sourcePath, destPath)
+    } else {
+      fs.copyFileSync(sourcePath, destPath)
+    }
+  }
+}
 
 async function insertDirectory() {
   const rootPath = vscode.workspace.workspaceFolders?.[0].uri.fsPath
@@ -8,15 +29,17 @@ async function insertDirectory() {
     vscode.window.showErrorMessage('No folder or workspace opened')
     return false
   }
-  const sourceDir = path.join(__dirname, 'pages/Edit') // 模板目录
-  const destDir = path.join(rootPath, 'src') // 目标目录
-  console.log(`检测到根目录为: ${sourceDir}, ${destDir}`)
-  // try {
-  //   await fs.copy(sourceDir, destDir)
-  //   vscode.window.showInformationMessage('Template directory copied successfully')
-  // } catch (err) {
-  //   vscode.window.showErrorMessage(`Failed to copy template directory: ${err.message}`)
-  // }
+  // Template Directory
+  let sourceDir = path.join(rootPath, 'src/library/components/Example/Edit')
+  // 模板目录不存在则插件里的代码
+  if (!sourceDir) {
+    sourceDir = path.join(__dirname, 'pages/Edit')
+  }
+
+  // Dest Directory
+  const destDir = path.join(rootPath, 'src/Edit')
+  console.log(`The directory is detected: ${sourceDir}, ${destDir}`)
+  copyDirectory(sourceDir, destDir)
   return true
 }
 
--- utils/command/insertDirectory/pages/FormEdit/api/index.js
+++
@@ -1,4 +0,0 @@
-import saveData from './saveData'
-import queryData from './queryData'
-
-export { saveData, queryData }
